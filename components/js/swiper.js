63dfcd3acab9d6c9222124e9c2ba0342cf5c1110
mohammad <mohammadmajidi27@gmail.com>
2022-05-20 09:48:54 +0430

    second commit

---
+++ components/js/swiper.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+let swiper2 = new Swiper(".Swiper", {
+    slidesPerView: 2,
+    // spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
