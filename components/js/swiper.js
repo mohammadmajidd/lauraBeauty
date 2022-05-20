494e3df6570b7f587a93992d2561d1027781c689
mohammad <mohammadmajidi27@gmail.com>
2022-05-20 12:16:13 +0430

    edit product swiper

--- components/js/swiper.js
+++ components/js/swiper.js
@@ -1,10 +1,37 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 5,
-    spaceBetween: 10,
+
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        540: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        576: {
+            slidesPerView:3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        720: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        },
+        960: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        },
+        1140: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        },
+        1320: {
+            slidesPerView: 5,
+            spaceBetween: 40
+        },
+    }
 });
 let swiper2 = new Swiper(".Swiper", {
     slidesPerView: 2,
